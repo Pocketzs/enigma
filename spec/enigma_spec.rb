cd618741b0263a4721fd6c87f898aa62fbb61d5d
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 19:33:47 -0600

    Add enigma encrypt method test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,18 @@ describe Enigma do
       expect(enigma).to be_a Enigma
     end
   end
+
+  describe 'encrypt' do
+    it 'returns a hash with a message key and date' do
+      enigma = Enigma.new
+
+      expected = {
+        encryption: 'keder ohulw',
+        key:        '02715',
+        date:       '040895'
+      }
+
+      expect(enigma.encrypt('hello world', '02715', '040895')).to eq(expected)
+    end
+  end
 end
