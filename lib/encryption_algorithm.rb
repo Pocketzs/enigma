ad9168f53e772dfcd557084a7aebfd082c7d894f
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 14:25:49 -0600

    Pass encryption algorithm shifts test

--- lib/encryption_algorithm.rb
+++ lib/encryption_algorithm.rb
@@ -1,3 +1,5 @@
+require './lib/shifts'
+
 class EncryptionAlgorithm
   attr_reader :message,
               :key,
@@ -9,5 +11,20 @@ class EncryptionAlgorithm
     @key            = attributes[:key]
     @date           = attributes[:date]
     @character_set  = ('a'..'z').to_a << ' '
+  end
+
+  # def encrypt_message
+  #   chars = @message.downcase.chars
+  #   message_index = chars.map do |char| 
+  #     next char unless character_set.index(char)
+  #     character_set.index(char)
+  #   end
+  #   encrypted_index = message_index.map do |int|
+
+  #   require 'pry'; binding.pry
+  # end
+
+  def shifts
+    Shifts.new(key: @key, date: @date).shifts
   end
 end
