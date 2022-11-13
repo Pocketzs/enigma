0075100cd178c5aa818413ab75e62b5fb64e008a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 20:24:30 -0600

    Fix typo in expected

--- spec/encryption_algorithm_spec.rb
+++ spec/encryption_algorithm_spec.rb
@@ -14,7 +14,7 @@ describe EncryptionAlgorithm do
 
       expected = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '
       ]
 
