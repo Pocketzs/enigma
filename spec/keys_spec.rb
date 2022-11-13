1c68b1534229252c12f6ab5a0716ee3a0386a03a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 21:25:00 -0600

    Remove a through b keys expects

--- spec/keys_spec.rb
+++ spec/keys_spec.rb
@@ -8,10 +8,6 @@ describe Keys do
 
       expect(keys).to be_a Keys
       expect(keys.five_digit_number.length).to eq 5
-      expect(keys.a_key).to eq 2
-      expect(keys.b_key).to eq 27
-      expect(keys.c_key).to eq 71
-      expect(keys.d_key).to eq 15
     end
   end
 end
