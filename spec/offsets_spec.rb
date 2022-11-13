2e71316c55d565dbd26d0817e4074e50cfda1cf6
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 00:03:46 -0600

    Add offsets date cypher test

--- spec/offsets_spec.rb
+++ spec/offsets_spec.rb
@@ -10,4 +10,12 @@ describe Offsets do
       expect(offsets.date).to eq '040895'
     end
   end
+
+  describe '#date_cypher' do
+    it 'sqaures the date and returns the last four digits' do
+      offsets = Offsets.new('040895')
+
+      expect(offsets.date_cypher).to eq '1025'
+    end
+  end
 end
