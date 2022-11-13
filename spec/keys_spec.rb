41b2d52f968997a33cea5d0e488d78363b780714
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 21:21:01 -0600

    Add keys existance and attributes test

---
+++ spec/keys_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/keys'
+
+Describe Keys do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      keys = Keys.new('02715')
+
+      expect(keys).to be_a Keys
+      expect(keys.five_digit_number.length).to eq 5
+      expect(keys.a_key).to eq 2
+      expect(keys.b_key).to eq 27
+      expect(keys.c_key).to eq 71
+      expect(keys.d_key).to eq 15
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,5 @@ SimpleCov.start
 
 require_relative 'enigma_spec'
 require_relative 'encryption_algorithm_spec'
+require_relative 'keys_spec'
 # Previous content of test helper now starts here
