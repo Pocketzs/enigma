5b4df75b38914fc07d78cff855da6c88cdc1bd56
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 23:27:26 -0600

    Add offsets spec to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,5 @@ SimpleCov.start
 require_relative 'enigma_spec'
 require_relative 'encryption_algorithm_spec'
 require_relative 'keys_spec'
+require_relative 'offsets_spec'
 # Previous content of test helper now starts here
