d879373322f280deb75b44141a0ab1e7494265ae
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 14:24:41 -0600

    Add more rigorous expectations to shifts shifts test

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -54,6 +54,7 @@ describe Shifts do
   describe '#shifts' do
     it 'adds the keys and offsets together to make a shifts hash' do
       shifts = Shifts.new({key: '02715', date: '040895'})
+      shifts2 = Shifts.new({key: '91034', date: '131122'})
 
       expected = {
         'A shift' => 3,
@@ -61,8 +62,15 @@ describe Shifts do
         'C shift' => 73,
         'D shift' => 20
       }
+      expected2 = {
+        'A shift' => 99,
+        'B shift' => 18,
+        'C shift' => 11,
+        'D shift' => 38
+      }
 
       expect(shifts.shifts).to eq expected
+      expect(shifts2.shifts).to eq expected2
     end
   end
 end
