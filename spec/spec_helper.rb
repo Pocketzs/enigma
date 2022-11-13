7a1642b88d2829736c4b46ea19afc87329dfd853
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 01:27:38 -0600

    Add shifts_spec to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@ require_relative 'enigma_spec'
 require_relative 'encryption_algorithm_spec'
 require_relative 'keys_spec'
 require_relative 'offsets_spec'
+require_relative 'shifts_spec'
 # Previous content of test helper now starts here
