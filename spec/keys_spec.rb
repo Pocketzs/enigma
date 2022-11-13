b88136ed2dc5903425c609f94186ea2dfa9ce467
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 23:12:03 -0600

    Add and pass keys different keys test

--- spec/keys_spec.rb
+++ spec/keys_spec.rb
@@ -12,13 +12,27 @@ describe Keys do
   end
   
   describe '#keys' do
-    it 'splits the digit number into 4 keys' do
+    it 'splits the digit number into keys' do
       keys = Keys.new('02715')
       expected = {
         'A key' => 02,
         'B key' => 27,
         'C key' => 71,
         'D key' => 15
+      }
+
+      expect(keys.keys.length).to eq 4
+      expect(keys.keys.values.all?(Integer)).to be true
+      expect(keys.keys).to eq expected
+    end
+
+    it 'can split a different digit number into keys' do
+      keys = Keys.new('91034')
+      expected = {
+        'A key' => 91,
+        'B key' => 10,
+        'C key' => 3,
+        'D key' => 34
       }
 
       expect(keys.keys.length).to eq 4
@@ -34,7 +48,6 @@ describe Keys do
 
       expect(keys1.digit_number.length).to eq 5
       expect(keys2.digit_number.length).to eq 6
-
       expect(keys1.lettered_keys).to eq ['A key', 'B key', 'C key', 'D key']
       expect(keys2.lettered_keys).to eq ['A key', 'B key', 'C key', 'D key', 'E key']
     end
