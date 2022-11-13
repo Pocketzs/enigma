0f285e6ce340dc45f13a376d9bd3eac1a714687b
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 14:44:57 -0600

    Pass encryption algorithm encrypt message test

--- lib/encryption_algorithm.rb
+++ lib/encryption_algorithm.rb
@@ -13,16 +13,18 @@ class EncryptionAlgorithm
     @character_set  = ('a'..'z').to_a << ' '
   end
 
-  # def encrypt_message
-  #   chars = @message.downcase.chars
-  #   message_index = chars.map do |char| 
-  #     next char unless character_set.index(char)
-  #     character_set.index(char)
-  #   end
-  #   encrypted_index = message_index.map do |int|
-
-  #   require 'pry'; binding.pry
-  # end
+  def encrypt_message
+    chars = @message.downcase.chars
+    message_index = chars.map do |char| 
+      next char unless character_set.index(char)
+      character_set.index(char)
+    end
+    shift_cycle = shifts.values.cycle
+    encrypted_message = message_index.map do |int|
+      next int unless int.class == Integer
+      int = @character_set[(shift_cycle.next+int) % 27]
+    end.join
+  end
 
   def shifts
     Shifts.new(key: @key, date: @date).shifts
