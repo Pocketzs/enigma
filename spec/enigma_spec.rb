cc59428766bc07b4f84a630549ed63f19db684ee
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 17:11:39 -0600

    Add enigma decrypt test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -37,4 +37,25 @@ describe Enigma do
       expect(enigma.encrypt_message('Hello World!', '02715', '040895')).to eq 'keder ohulw!' 
     end
   end
+
+  describe '#decrypt' do
+    it 'returns a hash with the encrypted message decrypted' do
+      enigma = Enigma.new
+
+      expected1 = {
+        encryption: 'hello world',
+        key:        '02715',
+        date:       '040895'
+      }
+      expected2 = {
+        encryption: 'my name is nigel',
+        key:        '02715',
+        date:       '040895'
+      }
+
+      expect(enigma.decrypt('keder ohulw', '02715', '040895')).to eq(expected1)
+      expect(enigma.decrypt('pysgdmxtlssglgxe', '02715', '040895')).to eq(expected2)
+    end
+  end
+
 end
