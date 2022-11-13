788530cd10f7debefdb7f3edcb4dcf4a94025744
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 01:10:14 -0600

    Pass offsets offsets test

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -9,6 +9,12 @@ class Offsets
     (date.to_i ** 2).to_s[-4..-1]
   end
 
+  def offsets
+    lettered_offsets.each_with_object({}).with_index do |(offset,hash), indx|
+      hash[offset] = date_cypher[indx].to_i
+    end
+  end
+
   def lettered_offsets
     lettered_offsets = []
     date_cypher.chars.each_with_index do |char, indx|
