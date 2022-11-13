0f006fd5b5591496e5e5da1cf447dd514135f7af
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 12:44:39 -0600

    Pass shifts keys test

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,3 +1,5 @@
+require './lib/keys'
+
 class Shifts
   attr_reader :key, :date
 
@@ -10,5 +12,9 @@ class Shifts
     [*0..3].map do |int|
       "#{(65+int).chr} shift"
     end
+  end
+
+  def keys
+    Keys.new(@key).keys
   end
 end
