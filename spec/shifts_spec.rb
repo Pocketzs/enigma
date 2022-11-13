94e681c114fa4862c1d962e08d0837fa2505da66
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 12:36:40 -0600

    Add shifts lettered shifts test

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -11,4 +11,13 @@ describe Shifts do
       expect(shifts.date).to eq '040895'
     end
   end
+
+  describe '#lettered_shifts' do
+    it 'returns an array of lettered shifts keys' do
+      shifts = Shifts.new({key: '02715', date: '040895'})
+
+      expect(shifts.lettered_shifts.length).to eq 4
+      expect(shifts.lettered_shifts).to eq ['A shift', 'B shift', 'C shift', 'D shift']
+    end
+  end
 end
