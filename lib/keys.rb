31aae2c05a6d5d8b012883ede5ca0c3d78e8a0b1
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 23:03:20 -0600

    Pass keys class keys and letted_keys method tests

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,7 +1,22 @@
 class Keys
-  attr_reader :five_digit_number
+  attr_reader :digit_number
 
-  def initialize(five_digit_number)
-    @five_digit_number = five_digit_number
+  def initialize(digit_number)
+    @digit_number = digit_number
+  end
+
+  def keys
+    lettered_keys.each_with_object({}).with_index do |(key, hash), indx|
+      hash[key] = digit_number[indx..(indx + 1)].to_i
+    end
+  end
+
+  def lettered_keys
+    lettered_keys = []
+    @digit_number.chars.each_with_index do |char, indx|
+      break if indx == @digit_number.length - 1
+      lettered_keys << "#{(65+indx).chr} key"
+    end
+    lettered_keys
   end
 end
