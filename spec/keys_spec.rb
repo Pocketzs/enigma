08b8744c7cac5489106c1e4c5ec7a7ba6bb23db0
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-12 21:48:03 -0600

    Add keys keys test

--- spec/keys_spec.rb
+++ spec/keys_spec.rb
@@ -10,4 +10,20 @@ describe Keys do
       expect(keys.five_digit_number.length).to eq 5
     end
   end
+  
+  describe '#keys' do
+    it 'splits the five digit number into 4 keys' do
+      keys = Keys.new('02715')
+      expected = {
+        'A key' => 02,
+        'B key' => 27,
+        'C key' => 71,
+        'D key' => 15
+      }
+      
+      expect(keys.keys.length).to eq 4
+      expect(keys.keys.values.all?(Integer)).to be true
+      expect(keys.keys).to eq expected
+    end
+  end
 end
