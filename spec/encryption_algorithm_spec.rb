e631c700679911a58b758bcf6126eddd638ef10e
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 13:17:58 -0600

    Add encryption algorithm attributes test

--- spec/encryption_algorithm_spec.rb
+++ spec/encryption_algorithm_spec.rb
@@ -4,13 +4,13 @@ require 'encryption_algorithm'
 describe EncryptionAlgorithm do
   describe '#initialize' do
     it 'exists' do
-      ea = EncryptionAlgorithm.new
+      ea = EncryptionAlgorithm.new({message: 'hello world', key: '02715', date: '040895'})
 
       expect(ea).to be_a EncryptionAlgorithm
     end
 
     it 'has a character set' do
-      ea = EncryptionAlgorithm.new
+      ea = EncryptionAlgorithm.new({message: 'hello world', key: '02715', date: '040895'})
 
       expected = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -21,6 +21,18 @@ describe EncryptionAlgorithm do
       expect(ea.character_set).to be_a Array
       expect(ea.character_set).to eq expected
       expect(ea.character_set.length).to eq 27
+    end
+
+    it 'has a message key and date' do
+      ea = EncryptionAlgorithm.new({message: 'hello world', key: '02715', date: '040895'})
+      ea2 = EncryptionAlgorithm.new({message: 'my name is nigel', key: '91034', date: '131122'})
+
+      expect(ea.message).to eq 'hello world'
+      expect(ea.key).to eq '02715'
+      expect(ea.date).to eq '040895'
+      expect(ea2.message).to eq'my name is nigel'
+      expect(ea2.key).to eq '91034'
+      expect(ea2.date).to eq '131122'
     end
   end
 end
