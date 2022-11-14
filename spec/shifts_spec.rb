3c2e42b5207e3011f3992008a16c65b66b5c1437
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 15:47:29 -0600

    Clean up style and spacing in shifts spec

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -25,7 +25,8 @@ describe Shifts do
     it 'returns a hash of keys' do
       shifts = Shifts.new({key: '02715', date: '040895'})
   
-      expected = {
+      expected = 
+      {
         'A key' => 2,
         'B key' => 27,
         'C key' => 71,
@@ -40,7 +41,8 @@ describe Shifts do
     it 'returns a hash of offsets' do
       shifts = Shifts.new({key: '02715', date: '040895'})
   
-      expected = {
+      expected = 
+      {
         'A offset' => 1,
         'B offset' => 0,
         'C offset' => 2,
@@ -56,13 +58,15 @@ describe Shifts do
       shifts = Shifts.new({key: '02715', date: '040895'})
       shifts2 = Shifts.new({key: '91034', date: '131122'})
 
-      expected = {
+      expected = 
+      {
         'A shift' => 3,
         'B shift' => 27,
         'C shift' => 73,
         'D shift' => 20
       }
-      expected2 = {
+      expected2 = 
+      {
         'A shift' => 99,
         'B shift' => 18,
         'C shift' => 11,
