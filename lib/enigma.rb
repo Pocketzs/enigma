debdd1caf137f5e37e21f66a21b5bd2264a95466
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-13 18:15:58 -0600

    Pass enigma decrypt and decrypt message tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,5 @@
 require './lib/encryption_algorithm'
+require './lib/decryption_algorithm'
 class Enigma
   def encrypt(message, key, date)
     {
@@ -18,5 +19,9 @@ class Enigma
       key:        key,
       date:       date
     }
+  end
+
+  def decrypt_message(message, key, date)
+    DecryptionAlgorithm.new(message: message, key: key, date: date).decrypt_message
   end
 end
