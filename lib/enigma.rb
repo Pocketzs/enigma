495eda6106211dd7ba782b85cae041010f91e2b6
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 17:17:33 -0600

    Refactor enigma to utilize shared class methods module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,8 @@ require './lib/encryption_algorithm'
 require './lib/decryption_algorithm'
 require './lib/generator'
 class Enigma
-  include Generator
+  include Generator,
+          SharedClassMethods
 
   def encrypt(message, key = rand_key, date = today)
     {
@@ -12,19 +13,11 @@ class Enigma
     }
   end
 
-  def encrypt_message(message, key, date)
-    EncryptionAlgorithm.new(message: message, key: key, date: date).encrypt_message
-  end
-
   def decrypt(message, key, date = today)
     {
       decryption: decrypt_message(message, key, date),
       key:        key,
       date:       date
     }
-  end
-
-  def decrypt_message(message, key, date)
-    DecryptionAlgorithm.new(message: message, key: key, date: date).decrypt_message
   end
 end
