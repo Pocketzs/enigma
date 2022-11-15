8053b131ccd929534ab0999b206bc08bc1fd7801
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 18:23:15 -0600

    Refactor cypher utilities lettered_hash_keys

--- lib/cypher_utilities.rb
+++ lib/cypher_utilities.rb
@@ -1,7 +1,15 @@
 module CypherUtilities
   def lettered_hash_keys
     [*0..3].map do |int|
-      "#{(65 + int).chr} #{self.class.to_s.downcase.chop}"
+      "#{(65 + int).chr} #{format_class_name}"
     end
+  end
+
+  def class_name
+    self.class.to_s
+  end
+
+  def format_class_name
+    class_name.downcase.chop
   end
 end
