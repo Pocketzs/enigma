dc42a35ce667950f89723ab7527ba4e4e3014265
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-15 02:59:14 -0600

    Add requires and include for crack algorithm class

--- lib/crack_algorithm.rb
+++ lib/crack_algorithm.rb
@@ -1,4 +1,11 @@
+require './lib/decryption_algorithm'
+require './lib/shared_class_methods'
+require './lib/generator'
+
 class CrackAlgorithm
+  include Generator
+  include SharedClassMethods
+
   attr_reader :message,
               :date
 
