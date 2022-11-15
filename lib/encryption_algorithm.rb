ad44810617aa4a09baafab2e7ef1ddee60f378d1
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 20:51:39 -0600

    Refactor encryption algorithm to use algorithm as a super class

--- lib/encryption_algorithm.rb
+++ lib/encryption_algorithm.rb
@@ -1,19 +1,8 @@
-require './lib/shifts'
-require './lib/shared_class_methods'
+require './lib/algorithm'
 
-class EncryptionAlgorithm
-  include SharedClassMethods
-
-  attr_reader :message,
-              :key,
-              :date,
-              :character_set
-  
+class EncryptionAlgorithm < Algorithm
   def initialize(attributes)
-    @message        = attributes[:message]
-    @key            = attributes[:key]
-    @date           = attributes[:date]
-    @character_set  = ('a'..'z').to_a << ' '
+    super
   end
 
   def encrypt_message
@@ -22,20 +11,5 @@ class EncryptionAlgorithm
       next int unless int.class == Integer
       @character_set[(sc.next + int) % 27]
     end.join
-  end
-
-  def message_chars
-    @message.downcase.chars
-  end
-
-  def message_index
-    message_chars.map do |char| 
-      next char unless character_set.index(char)
-      character_set.index(char)
-    end
-  end
-
-  def shift_cycle
-    shifts.values.cycle
   end
 end
