1ab66b23ecf92e7caf29b65e0fff7195d42eb140
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 20:43:37 -0600

    Pass all algorithm super class specs

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -29,12 +29,12 @@ describe Algorithm do
       a = Algorithm.new({message: 'keder ohulw', key: '02715', date: '040895'})
       a2 = Algorithm.new({message: 'pysgdmxtlssglgxe', key: '91034', date: '131122'})
 
-      expect(da.message).to eq 'keder ohulw'
-      expect(da.key).to eq '02715'
-      expect(da.date).to eq '040895'
-      expect(da2.message).to eq'pysgdmxtlssglgxe'
-      expect(da2.key).to eq '91034'
-      expect(da2.date).to eq '131122'
+      expect(a.message).to eq 'keder ohulw'
+      expect(a.key).to eq '02715'
+      expect(a.date).to eq '040895'
+      expect(a2.message).to eq'pysgdmxtlssglgxe'
+      expect(a2.key).to eq '91034'
+      expect(a2.date).to eq '131122'
     end
   end
 
@@ -43,7 +43,7 @@ describe Algorithm do
       it 'returns an array of the messages characters downcased' do
         a = Algorithm.new({message: 'keder ohulw!', key: '02715', date: '040895'})
 
-        expect(da.message_chars).to eq ['k', 'e', 'd', 'e', 'r', ' ', 'o', 'h', 'u', 'l', 'w', '!']
+        expect(a.message_chars).to eq ['k', 'e', 'd', 'e', 'r', ' ', 'o', 'h', 'u', 'l', 'w', '!']
       end
     end
 
@@ -51,7 +51,7 @@ describe Algorithm do
       it 'returns an array of the message characters index based on character set' do
         a = Algorithm.new({message: 'keder ohulw!', key: '02715', date: '040895'})
 
-        expect(da.message_index).to eq [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22, "!"]
+        expect(a.message_index).to eq [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22, "!"]
       end
     end
 
@@ -59,9 +59,9 @@ describe Algorithm do
       it 'loops the shift in a cycle' do
         a = Algorithm.new({message: 'keder ohulw!', key: '02715', date: '040895'})
         expected = [3, 27, 73, 20, 3, 27]
-        result = da.shift_cycle.first(6)
+        result = a.shift_cycle.first(6)
 
-        expect(da.shift_cycle).to be_a Enumerator
+        expect(a.shift_cycle).to be_a Enumerator
         expect(result).to eq expected
       end
     end
