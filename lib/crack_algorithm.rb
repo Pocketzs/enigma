116ae025ff26a282a201e9c4c2c3a159ce8cee23
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-15 03:05:31 -0600

    Pass crack message test

--- lib/crack_algorithm.rb
+++ lib/crack_algorithm.rb
@@ -14,7 +14,62 @@ class CrackAlgorithm
               :final_key_array
 
   def initialize(attributes)
-    @message  = attributes[:message]
-    @date     = attributes[:date]
+    @message          = attributes[:message]
+    @date             = attributes[:date]
+    @key              = '00000'
+    @counter          = 0
+    @upper_bound      = 99999
+    @final_key_array  = []
+  end
+
+  # def crack_message
+  #   until decrypt_message(@message, @key, @date)[-4..-1] == " end"
+  #     @key = format('%05d', @key.to_i + 1)
+  #   end
+  #   @final_key = @key
+  #   decrypt_message(@message, @key, @date)
+  # end
+
+  # def crack_message
+  #   until decrypt_message(@message, @key, @date)[-4..-1] == " end"
+  #     @key = rand_key
+  #   end
+  #   @final_key = @key
+  #   decrypt_message(@message, @key, @date)
+  # end
+
+  def crack_message
+    until end_check?
+      alternating_series
+    end
+    @final_key_array << @key
+    decrypt_message(@message, @key, @date)
+  end
+
+  def alternating_series
+    plus_minus_switch
+    incrementer
+  end
+
+  def plus_minus_switch
+    @key = format('%05d', @key.to_i + @upper_bound) if @counter.even?
+    @key = format('%05d', @key.to_i - @upper_bound) if @counter.odd?
+  end
+
+  def incrementer
+    @upper_bound -= 1
+    @counter += 1
+  end
+
+  def final_key
+    @final_key_array[0]
+  end
+
+  def end_check?
+    decrypt_last_four == " end"
+  end
+
+  def decrypt_last_four
+    decrypt_message(@message, @key, @date)[-4..-1]
   end
 end
